db3d702bc0050aebcad1789c027f01686feae5d7
고은정 <ejko0911@gmail.com>
2022-03-02 15:24:28 +0900

    Instagram-front 프로젝트 및 기본구조 생성

---
+++ instagram-front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Header />
+        <section>메인화면에 출력된 리스트</section>
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+    name: "App",
+    components: {
+        Header,
+        Footer,
+    },
+};
+</script>
