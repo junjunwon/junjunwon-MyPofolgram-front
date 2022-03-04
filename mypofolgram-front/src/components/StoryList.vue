8bb79af05316ff012dbca2c6ff4b6b6c064bcd57
고은정 <ejko0911@gmail.com>
2022-03-04 11:13:22 +0900

    스토리리스트 퍼블리싱

--- mypofolgram-front/src/components/StoryList.vue
+++ mypofolgram-front/src/components/StoryList.vue
@@ -2,9 +2,16 @@
     <div class="storyList">
         <div class="story">
             <img src="/images/example.jpeg" alt="고양이">
-            storylist 컴포넌트
+            <p>eunj_eong</p>
+        </div>
+        <div class="story">
+            <img src="/images/example.jpeg" alt="고양이">
+            <p>eunj_eong2</p>
+        </div>
+        <div class="story">
+            <img src="/images/example.jpeg" alt="고양이">
+            <p>long_eunj_eong2</p>
         </div>
     </div>
-    
 </template>
 
