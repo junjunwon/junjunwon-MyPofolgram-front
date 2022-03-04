86be4f5b363063e6b97296fa26687f51374406e6
고은정 <ejko0911@gmail.com>
2022-03-04 11:38:08 +0900

    Header 퍼블리싱

--- mypofolgram-front/src/components/Header.vue
+++ mypofolgram-front/src/components/Header.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Header</h1>
+    <div class="header">
+        <h1 class="logo">
+            <img src="/images/logo.png" alt="" />
+        </h1>
+        <div class="iconList">
+            <i class="fa-solid fa-house"></i>
+            <i class="fa-solid fa-paper-plane"></i>
+            <i class="fa-solid fa-square-plus"></i>
+            <i class="fa-solid fa-compass"></i>
+            <i class="fa-solid fa-heart"></i>
+            <img src="/images/example.jpeg" alt="유저이미지">
+        </div>
+    </div>
 </template>
