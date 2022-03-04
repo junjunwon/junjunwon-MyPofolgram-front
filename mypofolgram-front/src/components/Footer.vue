2ed146ce5fa3324318759b23cf52e045210148bd
고은정 <ejko0911@gmail.com>
2022-03-04 13:21:07 +0900

    footer 안쓰는 클래스 삭제

--- mypofolgram-front/src/components/Footer.vue
+++ mypofolgram-front/src/components/Footer.vue
@@ -1,10 +1,8 @@
 <template>
-    <div class="footerWrap">
-        <div class="footer">
-            <i class="fa-solid fa-house"></i>
-            <i class="fa-solid fa-magnifying-glass"></i>
-            <i class="fa-solid fa-clapperboard"></i>
-            <i class="fa-solid fa-circle-user"></i>
-        </div>
+    <div class="footer">
+        <i class="fa-solid fa-house"></i>
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <i class="fa-solid fa-clapperboard"></i>
+        <i class="fa-solid fa-circle-user"></i>
     </div>
 </template>
