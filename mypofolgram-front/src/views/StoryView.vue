6306ad62fee022804144db894a15e7c29ab0733c
고은정 <ejko0911@gmail.com>
2022-04-26 13:55:19 +0900

    스토리 이미지 출력 임시커밋

--- mypofolgram-front/src/views/StoryView.vue
+++ mypofolgram-front/src/views/StoryView.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>인스타 스토리 클릭시 출력 : {{ id }}</div>
+    <div class="storyView">
+        <!-- <div>인스타 스토리 클릭시 출력 : {{ id }}</div> -->
+        <img :src="imgUrl" alt="" />
+    </div>
 </template>
 
 <script>
@@ -7,6 +10,7 @@ export default {
     data() {
         return {
             id: "",
+            imgUrl: "",
         };
     },
     mounted() {
@@ -16,6 +20,15 @@ export default {
         getStoryId() {
             this.id = this.$route.params.id;
             console.log("스토리 id : " + this.id);
+
+            // 스토리 아이디에 대한 이미지? 동영상을 API로 가져오는게 맞지않나 생각중,,,,,,
+            if (this.id.length !== 0) {
+                this.getStoryUrl();
+            }
+        },
+        getStoryUrl() {
+            // 위 주석처럼 여기서 API호출?
+            this.imgUrl = "/images/example.jpeg";
         },
     },
 };
